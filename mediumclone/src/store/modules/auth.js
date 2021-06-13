be6d4dccf8dccb7789c8b592def261b4b19c27e4
Ralfik <developeraul@gmail.com>
2021-06-13 17:32:51 +0300

    создана коммуникация при логине

--- mediumclone/src/store/modules/auth.js
+++ mediumclone/src/store/modules/auth.js
@@ -1,4 +1,5 @@
 import authApi from '@/api/auth';
+
 import {setItem} from '@/helpers/persistanceStorage'
 
 const state = {
@@ -7,43 +8,90 @@ const state = {
   validationErrors: null,
   isLogedIn: null,
 };
+
+export const mutationTypes = {
+  registerStart: '[auth] registerStart',
+  registerSuccess: '[auth] registerSuccess',
+  registerFailure: '[auth] registerFailure',
+
+  loginStart: '[auth] loginStart',
+  loginSuccess: '[auth] loginSuccess',
+  loginFailure: '[auth] loginFailure',
+};
+
+export const actionTypes = {
+  register: '[auth] register',
+  login: '[auth] login'
+}
+
+
 const mutations = {
-  registerStart(state) {
+  [mutationTypes.registerStart](state) {
     state.isSubmitting = true;
     state.validationErrors = null;
   },
-  registerSuccess(state, payload) {
+  [mutationTypes.registerSuccess](state, payload) {
     state.isSubmitting = false;
     state.currentUser = payload;
     state.isLogedIn = true;
   },
-  registerFailure(state, payload) {
+  [mutationTypes.loginFailure](state, payload) {
     state.isSubmitting = false;
     state.validationErrors = payload;
-    //state.isLogedIn = false;
+  },
+
+  [mutationTypes.loginStart](state) {
+    state.isSubmitting = true;
+    state.validationErrors = null;
+  },
+  [mutationTypes.loginSuccess](state, payload) {
+    state.isSubmitting = false;
+    state.currentUser = payload;
+    state.isLogedIn = true;
+  },
+  [mutationTypes.loginFailure](state, payload) {
+    state.isSubmitting = false;
+    state.validationErrors = payload;
   },
 };
 
 const actions = {
-  register(context, credentials) {
+  [actionTypes.register](context, credentials) {
     return new Promise(resolve => {
-      context.commit('registerStart')
+      context.commit(mutationTypes.registerStart)
       authApi
         .register(credentials)
         .then(response => {
-          context.commit('registerSuccess', response.data.user);
+          context.commit(mutationTypes.registerSuccess, response.data.user);
           setItem('accessTocen', response.data.user.token)
           resolve(response.data.user)
 
         })
         .catch(result => {
-          context.commit('registerFailure', result.response.data.errors)
+          context.commit(mutationTypes.registerFailure, result.response.data.errors)
           console.log('result errors', result);
         });
     });
+
     // setTimeout(() => {
     //   context.commit('registerStart');
     // }, 1000);
+  },
+  [actionTypes.login](context, credentials){
+    return new Promise(resolve =>{
+      context.commit(mutationTypes.loginStart)
+      authApi
+        .login(credentials)
+        .then(response =>{
+          context.commit(mutationTypes.loginSuccess, response.data.user);
+          setItem('accessTocen', response.data.user.token)
+          resolve(response.data.user)
+        })
+        .catch(result=>{
+          context.commit(mutationTypes.loginFailure, result.response.data.errors)
+          console.log('result errors', result);
+        })
+    })
   }
 };
 
---
+++ mediumclone/src/views/Login.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="auth-page ng-scope">
+        <div class="container page">
+            <div class="row">
+            <div class="col-md-6 offset-md-3 col-xs-12">
+                <h1 class="text-xs-center ng-binding" >Sign in</h1>
+                <p class="text-xs-center">
+                    <router-link :to="{name: 'register'}">
+                        Need an account?
+                    </router-link>
+                </p>
+                <McvValidationError 
+                    v-if="validationErrors"
+                    :validationErrors="validationErrors"
+                    />
+
+                <form 
+                    @submit.prevent="onSubmit" 
+                    class="ng-pristine ng-valid ng-valid-email"
+                >
+                    <fieldset class="form-group" >
+                    <input 
+                        class="form-control form-control-lg" 
+                        type="text" 
+                        placeholder="Email" 
+                        v-model="email"
+                    >
+                    </fieldset>
+                    <fieldset class="form-group" >
+                    <input 
+                        class="form-control form-control-lg" 
+                        type="password" 
+                        placeholder="Password" 
+                        v-model="password"
+                    >
+                    </fieldset>
+                    <button 
+                        class="btn btn-lg btn-primary pull-xs-right"
+                        :disabled="isSubmitting"
+                    >Sign in
+                    </button>
+                </form>
+                {{isSubmitting}}
+            </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import McvValidationError from '@/components/ValidationErrors';
+import {actionTypes} from '@/store/modules/auth'
+import { mapState } from 'vuex';
+export default {
+    name: 'McvRegister',
+    components: {
+        McvValidationError
+    },
+    data(){
+        return{
+            email: '',
+            password: '',
+        }
+    },
+    computed:{
+        ...mapState({
+            isSubmitting: (state)=>state.auth.isSubmitting,
+            validationErrors: (state)=>state.auth.validationErrors,
+        })
+        // isSubmitting(){
+            
+        //     return this.$store.state.auth.isSubmitting;
+        // },
+        // validationErrors(){
+        //     return this.$store.state.auth.validationErrors;
+        // }
+    },
+    methods: {
+        onSubmit(){
+            console.log("submited");
+            this.$store.dispatch (actionTypes.login,
+                {
+                    email: this.email,
+                    password: this.password 
+                })
+                .then(()=>{
+                    this.$router.push({name: 'home'});
+                })
+
+        },
+        
+    },
+}
+</script>
+
+<style>
+
+</style>
