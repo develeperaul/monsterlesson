bbafbdc970356ab17590440fc295787e1ea62a99
Ralfik <developeraul@gmail.com>
2021-06-15 12:24:13 +0300

    api для загрузки списка постов

--- mediumclone/src/api/axios.js
+++ mediumclone/src/api/axios.js
@@ -5,9 +5,7 @@ axios.defaults.baseURL = 'https://conduit.productionready.io/api';
 
 axios.interceptors.request.use(config => {
     const token = getItem('accessToken');
-    console.log(token)
     const authorizationToken = token ? `Token ${token}` : '';
-    console.log(authorizationToken)
     
     config.headers.Authorization = authorizationToken
     return config
---
+++ mediumclone/src/api/feed.js
@@ -0,0 +1,10 @@
+import axios from '@/api/axios';
+
+const getFeed = apiURL =>{
+    
+    return axios.get(apiURL)
+}
+
+export default {
+    getFeed
+}
