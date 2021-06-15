02566109ddfb307d058c2ba2bf756cacac759808
Ralfik <developeraul@gmail.com>
2021-06-15 12:25:02 +0300

    создание state для списка постов

--- mediumclone/src/store/index.js
+++ mediumclone/src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import auth from '@/store/modules/auth';
+import feed from '@/store/modules/feed';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -9,6 +10,7 @@ export default new Vuex.Store({
   mutations: {},
   actions: {},
   modules: {
-    auth
+    auth,
+    feed
   }
 });
